f753e77003bdbb4a7381a8aa2597a85c2d8b3dce
pavansingh888 <93345404+pavansingh888@users.noreply.github.com>
2024-03-28 18:45:50 +0000

    Operations

--- 01_Basics/03_ConversionsOperations.js
+++ 01_Basics/03_ConversionsOperations.js
@@ -1,4 +1,4 @@
-//      after conversion ValueInNum   -    Type
+//      after conversion ValueInNum   -    Type=Number
 //let score = 56        //number      
 //let score = "56"      //number
 //let score = "56abc"   //NaN
@@ -32,5 +32,49 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue); //OP -3
+
+// console.log(2+2); //OP 4
+// console.log(2-2); //OP 0
+// console.log(2*2); //OP 4
+// console.log(2**3);//OP 8 - 2 to pow 3
+// console.log(2/3); //OP 0.6666666666
+// console.log(2%3); //OP 2
+
+let str1 = "pavan"
+let str2 = " singh"
+
+let str3 = str1 + str2
+// console.log(str3); //OP pavan singh
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log( (3 + 4) * 5 % 3); //using bracket is prefered
+
+// console.log(+true); //1
+// console.log(+"");   //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);   //101
+console.log(gameCounter++); //101
+console.log(gameCounter);   //102
+console.log(++gameCounter); //103
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
