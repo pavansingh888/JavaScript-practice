bcd6caeb908f998fd48370f2592a7e65072f6581
pavansingh888 <93345404+pavansingh888@users.noreply.github.com>
2024-04-04 18:15:05 +0000

    Scope level and mini hoisting

--- 03_basics/02_Scopes.js
+++ 03_basics/02_Scopes.js
@@ -18,3 +18,49 @@ if (true) {
 
 //core scope inside chrome dev tools is different and example run thru node have different scope. #Interview
 
+function one(){
+    const username = "hitesh"
+
+    //child function can access variable of parent functions
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website); //not defined
+
+     two() //hitesh
+
+}
+
+// one()
+
+
+//Similarly for if-else
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website); //hitesh youtube
+    }
+    // console.log(website); // not defined - scope error
+}
+
+// console.log(username); //  not defined - scope error
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+// console.log(addone(5)) //6  - this is possible
+
+function addone(num){
+    return num + 1
+}
+
+// console.log(addone(5)) //6
+
+addTwo(5) //ReferenceError: Cannot access 'addTwo' before initialization -- due to concept of hoisting, how Fx is placed , declared and thier execution context, how JS treat variables n all.
+const addTwo = function(num){ //it can also be called as expression instead of funtion although addTwo is a funtion.
+
+    return num + 2
+}
