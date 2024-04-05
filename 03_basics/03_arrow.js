e46a5fdf80f838770e01d689516b10d099cf96db
pavansingh888 <93345404+pavansingh888@users.noreply.github.com>
2024-04-05 18:29:25 +0000

    this & Arrows

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,67 @@
+const user= {
+    username: "Pavan",
+    price: 999,
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to website`); // to refer current context
+        console.log(this);  /*Pavan, welcome to website
+        {
+          username: 'Pavan',
+          price: 999,
+          welcomeMessage: [Function: welcomeMessage]
+        }
+        sam, welcome to website
+        {
+          username: 'sam',
+          price: 999,
+          welcomeMessage: [Function: welcomeMessage]
+        } */
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this); //{}  - refering to node environment, global scope currently don't have any variable. If we use above line of code in chrome console we will get window object as global object. since prevously we only had JS engine in browser. #Interview
+
+
+// function chai(){
+//     let username="pavan"
+//     console.log(this.username); //undefined - so context is only working for objects but not for function
+// }
+
+// chai()
+
+
+// const chai = function(){
+//     let username="singh"
+//     console.log(this.username); //undefined
+// }
+
+
+const chai = () => {
+    let username="singh"
+    console.log(this.username); //undefined
+    console.log(this); //{}
+}
+// chai()
+
+
+// const addTwo = (num1,num2) => {
+//     return num1+num2
+// }
+
+// console.log(addTwo(3,4)); //7
+
+
+// const addTwo = (num1,num2) => num1+num2 //implicit return
+
+// const addTwo = (num1,num2) => ( num1+num2 ) //for () return keyword is not needed, for {} return keyword is needed
+
+// const addTwo = (num1,num2) => {username:"pavan" } //to return object we need to use () //undefined
+
+
+const addTwo = (num1,num2) =>( {username:"pavan" } )
+console.log(addTwo(3,4)); //{ username: 'pavan' }
+
+
